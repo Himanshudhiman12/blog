5dd3424e7a1fd3e59f376f9acbb816bcab09d411
himanshu <himanshu.dhiman@scopeworker.com>
2020-04-06 12:59:35 +0530

    removed connection for mongo

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 var app = express();
 bodyParser = require('body-parser')
 app.use(bodyParser.json());
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/blogDB", function (err) { 
-   if (err){
-       console.log("DB Error: ", err);
-       process.exit(1);
-   } else {
-       console.log('MongoDB Connected');
-   }
-});
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb://localhost/blogDB", function (err) { 
+//    if (err){
+//        console.log("DB Error: ", err);
+//        process.exit(1);
+//    } else {
+//        console.log('MongoDB Connected');
+//    }
+// });
 app.get('/', function (req, res) {
     res.send('hello world');
    });
